03888118681e22575bad2aae4934035ffe16c34a
rahuldevhub <iamrahulrajendran01@gmail.com>
2025-02-28 13:53:01 +0530

    28.3.7

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -34,7 +34,7 @@ function About() {
             <img src={laptopImg} alt="about" className="img-fluid" />
           </Col>
         </Row> */}
-
+        
         <h1 className="project-heading">
           Professional <strong className="purple">Skillset </strong>
         </h1>
